bfe88acb5aa073b96b485f3036de1564ec608699
vinuthas2005 <vinuthas23csds@rnsit.ac.in>
2026-07-10 21:20:12 +0530

    added frontend url

--- api/main.py
+++ api/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5175",
         "http://127.0.0.1:5175",
+        "https://traffic-congestion-prediction.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
